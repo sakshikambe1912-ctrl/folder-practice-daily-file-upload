3ec2be513658203a7d85940ceaa12692ac4cc7b0
sakshikambe1912-ctrl <sakshikambe1912@gmail.com>
2026-07-08 16:18:18 +0530

    Add files via upload

--- Question1_8thjuly.cs
+++ Question1_8thjuly.cs
@@ -1,33 +1,33 @@
 
 
 
-    // long[] sales = {10000,12000,14000,17000,15000,19000};
+    long[] sales = {10000,12000,14000,17000,15000,19000};
 
-    // long totalsales=0;
-    // long highest = sales[0];
-    // long lowest = sales[0];
-    // for(long i = 1; i < sales.Length; i++)
-    //     {
-    //         if (sales[i] > highest)
-    //             {
-    //                 highest=sales[i];
-    //             }
-    //         if (sales[i] < lowest)
-    //             {
-    //                 lowest=sales[i];
-    //             }
+    long totalsales=0;
+    long highest = sales[0];
+    long lowest = sales[0];
+    for(long i = 1; i < sales.Length; i++)
+        {
+            if (sales[i] > highest)
+                {
+                    highest=sales[i];
+                }
+            if (sales[i] < lowest)
+                {
+                    lowest=sales[i];
+                }
 
-    //     }
+        }
     
 
-    // foreach(long s in sales)
-    // {
-    //    Console.WriteLine(s); 
-    //    totalsales+=s;
+    foreach(long s in sales)
+    {
+       Console.WriteLine(s); 
+       totalsales+=s;
        
-    // }
-    // Console.WriteLine("Total sales:"+totalsales);
-    // Console.WriteLine("Average sales:"+totalsales/6);
-    // Console.WriteLine("the Heighest Sale is:"+highest);
-    // Console.WriteLine("the lowest Sale is:"+lowest);
+    }
+    Console.WriteLine("Total sales:"+totalsales);
+    Console.WriteLine("Average sales:"+totalsales/6);
+    Console.WriteLine("the Heighest Sale is:"+highest);
+    Console.WriteLine("the lowest Sale is:"+lowest);
     
