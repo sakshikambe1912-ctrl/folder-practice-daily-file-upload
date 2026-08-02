47901064771a5c323a4a5fddbeab7cc49ed4eb96
sakshikambe1912-ctrl <sakshikambe1912@gmail.com>
2026-08-02 23:28:06 +0530

    Add files via upload

---
+++ 27july_Product/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _27julyProduct.Models
+{
+    public class Product
+    {
+        [Required(Error Message="Id of the product is required.")]
+        [Range(3,100000,ErrorMessage ="the Id of product must lie between the 3 and 100000.")]
+        public int Id {  get; set; }
+
+        [Required(Error Message = "Name of product is required.")]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="Name must have maximum of 20 and minimum of 3 characters.")]
+        public string Name { get; set; }
+
+        [Required(Error Message = "Quantity of the product is required.")]
+        public int Quantity { get; set; }
+
+        [Required(Error Message = "Price of the product is required.")]
+        public decimal Price { get; set; }
+
+
+
+    }
+}
---
+++ 27july_Product/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using _27julyProduct.Models;
+
+namespace _27julyProduct.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product
+            {
+                Id=1,
+                Name="Laptop",
+                Quantity= 1,
+                Price=78000
+            },
+            new Product
+            {
+                Id=2,
+                Name="Keyboard",
+                Quantity= 8,
+                Price=8000
+            },
+            new Product
+            {
+                Id=3,
+                Name="Mouse",
+                Quantity= 20,
+                Price=200
+            },
+            new Product
+            {
+                Id=4,
+                Name="Earphone",
+                Quantity= 40,
+                Price=2000
+            }
+
+        };
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(products);
+        }
+        [HttpGet("(id)")]
+        public IActionResult GetProducts(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+                return NotFound();
+            return Ok(product);
+
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+            return CreatedAtAction(nameof(GetProducts),
+                new { id = product.Id }, product);
+
+
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product upadateproduct)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+                return NotFound();
+
+            product.Quantity = upadateproduct.Quantity;
+            return NoContent();
+
+
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+                return NotFound();
+            products.Remove(product);
+            return NoContent();
+        }
+
+    }
+}
